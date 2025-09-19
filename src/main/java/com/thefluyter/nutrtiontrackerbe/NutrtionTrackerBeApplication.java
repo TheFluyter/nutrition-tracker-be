ac46d613d19a6572b426b97c821588a70118eef4
rickderuiter <rickderuiter@protonmail.com>
2025-09-19 20:12:04 +0200

    initial commit

---
+++ src/main/java/com/thefluyter/nutrtiontrackerbe/NutrtionTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.thefluyter.nutrtiontrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrtionTrackerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrtionTrackerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thefluyter/nutrtiontrackerbe/NutrtionTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefluyter.nutrtiontrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrtionTrackerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
